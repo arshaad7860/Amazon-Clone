03981344987c071e234114b39ac0039f96f2ada1
Mohamed <arshaad7860@hotmail.com>
2020-10-26 16:42:47 +0200

    Everything Completed

--- amazon-clone/src/axios.js
+++ amazon-clone/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/mo-clone-7860/us-central1/api", // the api url or cloud function
+  baseURL: "https://us-central1-mo-clone-7860.cloudfunctions.net/api", // the api url or cloud function
 });
 
 export default instance;
